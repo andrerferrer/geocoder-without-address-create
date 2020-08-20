82ea8d621ea6a34b251938f173600062b39711fa
Andre Ferrer <andrerferrer@hotmail.com>
2020-08-20 14:14:36 -0300

    implement callbacks on restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -8,11 +8,14 @@ class Restaurant < ApplicationRecord
   has_many :reviews
 
   geocoded_by :address
+
+  # This is going to make the address be saved before geocoding
+  before_validation :generate_and_save_address
   # This is going to make geocode run after validation
   # Check AR callbacks -> https://guides.rubyonrails.org/active_record_callbacks.html
   after_validation :geocode
 
-  def address
-     "#{street}, #{city}, #{country}"
+  def generate_and_save_address
+     self.address = "#{street}, #{city}, #{country}"
   end
 end
