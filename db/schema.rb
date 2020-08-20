771b7c090762a5ad8d3dfbee86149136e669fa78
Andre Ferrer <andrerferrer@hotmail.com>
2020-08-20 14:14:28 -0300

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,7 @@ ActiveRecord::Schema.define(version: 2020_08_20_164332) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
+    t.string "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.float "latitude"
